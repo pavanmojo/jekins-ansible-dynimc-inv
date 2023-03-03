e9da0bb9545d2357dababc413f98f39afcb8ecc8
Pavan b Rao <122536851+pavanmojo@users.noreply.github.com>
2023-03-03 15:03:17 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -1,16 +1,14 @@
 provider "aws"{
-  region = "ap-southeast-2"
+region = "ap-south-1"
 }
-
-
-resource "aws_instance" "AWSInstance"{
-    ami = "ami-08cd0547bf85331e7"
-	instance_type = "t2.micro"
-	key_name = "devopstrainingmss"
-	security_groups = ["launch-wizard-2"]
-	tags = {
-	 Name = "tomcatservers"
-	}
+resource "aws_instance" "AWSInstance" {
+ami = "ami-0e07dcaca348a0e68"
+instance_type = "t2.micro"
+key_name = "pavanbs"
+security_groups = ["launch-wizard-2"]
+tags = {
+Name = "tomcatservers"
+}
 }
 
 
